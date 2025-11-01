3bdcb6065935b2553939841d5ed47a028888256a
Ana62-mp <ana.moposita84@gmail.com>
2025-10-31 20:01:09 -0500

    script reto 2

--- scripts/script1.sql
+++ scripts/script1.sql
@@ -6,3 +6,29 @@ create table productos(
     stock int not null,
 	constraint productos_pk primary key(codigo)
 )
+
+insert into productos(codigo,nombre,descripcion,precio,stock)
+values(1,'Pañuelos','Aroma a menta',2.24,45)
+
+insert into productos(codigo,nombre,descripcion,precio,stock)
+values(2,'Hojas','De cuadros',3.50,23)
+
+insert into productos(codigo,nombre,descripcion,precio,stock)
+values(3,'Manteles','Absorbentes para cocina',5.58,96)
+
+insert into productos(codigo,nombre,descripcion,precio,stock)
+values(4,'Cuadernos','Pasta dura',6.72,67)
+
+insert into productos(codigo,nombre,descripcion,precio,stock)
+values(5,'Pegamento',1.45,71)
+
+insert into productos(codigo,nombre,precio,stock)
+values(6,'Toallas',7.89,10)
+
+insert into productos(codigo,nombre,precio,stock)
+values(7,'Resaltadores',5.62,17)
+
+insert into productos(codigo,nombre,precio,stock)
+values(8,'Cartulinas',2.37,28)
+
+select * from productos
--- scripts/script4.sql
+++ scripts/script4.sql
@@ -5,3 +5,35 @@ create table registros_entrada(
     hora time not null,
     constraint registros_entrada_pk primary key(codigo_registro)
 )
+
+insert into registros_entrada(codigo_registro,cedula_empleado,fecha,hora)
+values(1,'1748547985','12/10/2024','08:00')
+
+insert into registros_entrada(codigo_registro,cedula_empleado,fecha,hora)
+values(2,'1784596875','12/10/2024','08:30')
+
+insert into registros_entrada(codigo_registro,cedula_empleado,fecha,hora)
+values(3,'1785425869','12/10/2024','08:45')
+
+insert into registros_entrada(codigo_registro,cedula_empleado,fecha,hora)
+values(4,'1748527458','12/10/2024','08:56')
+
+insert into registros_entrada(codigo_registro,cedula_empleado,fecha,hora)
+values(5,'1744789652','12/10/2024','09:20')
+
+insert into registros_entrada(codigo_registro,cedula_empleado,fecha,hora)
+values(6,'0147859638','14/10/2024','14:50')
+
+insert into registros_entrada(codigo_registro,cedula_empleado,fecha,hora)
+values(7,'1744772258','15/10/2024','08:48')
+
+insert into registros_entrada(codigo_registro,cedula_empleado,fecha,hora)
+values(8,'1742258964','15/10/2024','10:54')
+
+insert into registros_entrada(codigo_registro,cedula_empleado,fecha,hora)
+values(9,'1745237747','15/10/2024','07:05')
+
+insert into registros_entrada(codigo_registro,cedula_empleado,fecha,hora)
+values(10,'1748569874','20/10/2024','09:10')
+
+select * from registros_entrada
